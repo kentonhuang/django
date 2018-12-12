a978fda72332be3aec86141edd78f7339d08798b
Kenton Huang <huangkenton@gmail.com>
2018-12-11 16:35:23 -0800

    home page shows posts

--- django/redditclone/reddit/posts/views.py
+++ django/redditclone/reddit/posts/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 
 # Create your views here.
 @login_required
 def create(request):
   if request.method == 'POST':
     if request.POST['title'] and request.POST['url']:
-      post = models.Post()
+      post = Post()
       post.title = request.POST['title']
-      post.url = request.POST['url']
+      if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+        post.url = request.POST['url']
+      else:
+        post.url = 'http://' + request.POST['url']
       post.pub_date = timezone.datetime.now()
       post.author = request.user
       post.save()
+      return redirect('home')
     else:
       return render(request, 'posts/create.html', {'error': 'ERROR: You must include a title and a URL to create a post.'})
   else:
     return render(request, 'posts/create.html')
+
+def home(request):
+  posts =  Post.objects.order_by('votes_total')
+  return render(request, 'posts/home.html', {'posts': posts})
